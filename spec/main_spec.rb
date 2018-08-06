ecad110468d163d817baf1b3a93cee892cb57a3f
Stefan Horning <stefan@hornings.de>
2018-08-06 16:53:01 +0200

    Fix and reanrangement of variables

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,4 +4,8 @@ describe "Clickhouse Setup" do
   describe package('clickhouse-server') do
     it { should be_installed }
   end
+
+  describe file('/etc/clickhouse-server/config.xml') do
+    its(:content) { should include("<path>#{ANSIBLE_VARS.fetch('clickhouse_path_data', 'FAIL')}</path>") }
+  end
 end
